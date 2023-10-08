d78dae619333187d24cce4bc819589ae38eda022
Shivansh Singh <singhshivansh12may@gmail.com>
2023-10-08 11:10:51 +0530

    Added booking database

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 const express =require('express');
-const {port}=require("./config/serverConfig")
+const bodyParser =require('body-parser')
+const {port}=require('./config/serverConfig')
+const apiRoutes=require('./routes/index.js')
+const db=require('./models/index')
 const app=express();
 
 const setupAndStart=()=>{
+    
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}));
+    app.use('/api',apiRoutes);  
     app.listen(port,()=>{
+        if(process.env.DB_SYNC){
+            db.sequelize.sync({alert:true});
+          }
         console.log("Server Started at "+port)
         
     });
